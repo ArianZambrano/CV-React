e316d212d592e9801c842730f490d6b3eb2acb66
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2020-02-18 23:21:36 -0500

    Prueba React Hooks

---
+++ src/components/Prueba.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Prueba extends Component{
+
+    state = {
+        active: true,
+    } 
+
+    handleClick = () => {
+        this.setState({
+            active = !this.state.active,
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.handleClick}>Ocultar</button>
+                {this.state.active &&
+                    <h1>Hola Mundo</h1>
+                }
+            </div>
+        );
+    }
+}
+
+export default Prueba;
