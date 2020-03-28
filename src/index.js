b13f66acf0fa9d3e95bada929bdaf2dee13a9132
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2020-03-28 00:34:57 -0500

    Eliminacion de prueba React Hooks

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import Prueba from './components/Prueba';
-
-ReactDOM.render(<Prueba />, document.getElementById('app'));
